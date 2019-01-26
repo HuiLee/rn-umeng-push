6d22b1617f61934ba7468dc9eee10eb50ddc5cde
Hui Lee <lihui870920@gmail.com>
2019-01-26 22:04:34 +0800

    配置友盟推送

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import PushUtil from './native/PushUtil'
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.state = {tag: "running_lee_top", result: "结果..."};
+        this.onPush()
+    }
+
+    onPush() {
+        PushUtil.addAlias(this.state.tag, "hfzd", (code, res) => {
+            if (code == 200) {
+                this.setState({result: code});
+            }
+            console.log(res)
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.instructions}>{this.state.result}</Text>
+                <TouchableOpacity
+                    style={styles.u_c_item}
+                    onPress={() => {
+                        this.onPush()
+                    }}
+                >
+                    <Text style={styles.welcome}>Welcome to React Native!</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
---
+++ ios/hfzd/AppDelegate.m
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import "RNUMConfigure.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [UMConfigure setLogEnabled:YES];
+  [RNUMConfigure initWithAppkey:@"5c4bd2ddb465f5ee9c000d0a" channel:@"App Store"];
+  
+  NSURL *jsCodeLocation;
+
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"hfzd"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+  rootView.backgroundColor = [UIColor blackColor];
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  return YES;
+}
+
+@end
